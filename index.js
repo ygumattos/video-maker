96a0a8e08bf8b45730b8d8762148d8c36a8150ff
Ygor Mattos <ygormattos.b@gmail.com>
2021-03-02 18:14:12 -0300

    feat: add Watson integration

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const robots = {
 }
 
 async function start() {
-    const content = {}
+    const content = {
+        maximumSentences: 7
+    }
     content.searchTerm = askAndReturnSearchTerm()
     content.prefix = askAndReturnPrefixTerm()
 
@@ -22,7 +24,7 @@ async function start() {
         return selectedPrefixText
     }
 
-    console.log(content)
+    console.log(JSON.stringify(content))
 }
 
 start()
