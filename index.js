b48f5bf452bb3ec4e0091729e1cce01961bad0b9
Ygor Mattos <ygormattos.b@gmail.com>
2021-03-05 08:23:46 -0300

    feat: connect with google cloud

--- index.js
+++ index.js
@@ -2,13 +2,15 @@
 const robots = {
     input: require('./robots/input'),
     text: require('./robots/text'),
-    state: require('./robots/states')
+    state: require('./robots/states'),
+    image: require('./robots/image')
 }
 
 async function start() {
 
     robots.input()
     await robots.text()
+    // await robots.image()
 
     const content = robots.state.load()
     console.dir(content, { depth: null })
---
+++ robots/image.js
@@ -0,0 +1,7 @@
+const state = require('./states')
+
+async function robot() {
+    const content = state.load()
+}
+
+module.exports = robot
