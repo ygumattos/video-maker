279cdf9e0b89e0e93c52574ccaf5085e31dc6a67
Ygor Mattos <ygormattos.b@gmail.com>
2021-03-08 23:52:13 -0300

    feat: add google images search

--- robots/image.js
+++ robots/image.js
@@ -1,7 +1,40 @@
+const { google } = require('googleapis')
+const customSearch = google.customsearch('v1')
+
 const state = require('./states')
+const googleSearchCredentials = require('../credentials/google-search.json')
 
 async function robot() {
     const content = state.load()
+
+    await fetchImagesOfAllSentences(content)
+
+    state.save(content)
+
+    async function fetchImagesOfAllSentences(content) {
+        for (const sentence of content.sentences) {
+            const query = `${content.searchTerm} ${sentence.keywords[0]}}`
+            sentence.images = await fetchGoogleAndReturnImagesLinks(query)
+
+            sentence.googleSearchQuery = query
+        }
+    }
+
+    async function fetchGoogleAndReturnImagesLinks(query) {
+
+        const response = await customSearch.cse.list({
+            auth: googleSearchCredentials.apiKey,
+            cx: googleSearchCredentials.searchEngineId,
+            q: query,
+            searchType: 'image',
+            // imgSize: 'huge',
+            num: 2
+        })
+
+        const imagesUrl = response.data.items.map(item => item.link)
+        return imagesUrl
+    }
+
 }
 
 module.exports = robot
